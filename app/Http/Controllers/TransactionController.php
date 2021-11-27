e3d72d486d4b0b8601502729421aa17135ad10ea
Febi Sepbriansyah <70487875+syahbi25@users.noreply.github.com>
2021-11-27 17:58:45 +0700

    Crud API

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Transaction;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class TransactionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $transaction = Transaction::orderBy('time', 'DESC')->get();
+        $response = [
+            'message' => 'List transactions order by time',
+            'data' => $transaction
+        ];
+
+        return response()->json($response, 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => ['required'],
+            'amount' => ['required', 'numeric'],
+            'type' => ['required', 'in:expense,revenue']
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        try {
+            $transaction = Transaction::create($request->all());
+            $response = [
+                'message' => 'transaction created',
+                'data' => $transaction
+            ];
+
+            return response()->json($response, 201);
+
+        } catch (QueryException $e) {
+
+            return response()->json([
+                'message' => "Failed" . $e->errorInfo
+            ]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $transaction = Transaction::findOrFail($id);
+        $response = [
+            'message' => 'Detail of transaction resource',
+            'data' => $transaction
+        ];
+
+        return response()->json($response, 200);
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $transaction = Transaction::findOrFail($id);
+
+        $validator = Validator::make($request->all(), [
+            'title' => ['required'],
+            'amount' => ['required', 'numeric'],
+            'type' => ['required', 'in:expense,revenue']
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        try {
+            $transaction->update($request->all());
+            $response = [
+                'message' => 'update transaction success',
+                'data' => $transaction
+            ];
+
+            return response()->json($response, 200);
+
+        } catch (QueryException $e) {
+
+            return response()->json([
+                'message' => "Failed" . $e->errorInfo
+            ]);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $transaction = Transaction::findOrFail($id);
+
+        try {
+            $transaction->delete();
+            $response = [
+                'message' => 'delete transaction success'
+            ];
+
+            return response()->json($response, 200);
+
+        } catch (QueryException $e) {
+
+            return response()->json([
+                'message' => "Failed" . $e->errorInfo
+            ]);
+        }
+    }
+}
